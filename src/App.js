1b70e7fc3f1bc3c65f657d5771c4cb32eecfaa00
eunbin777 <103042314+eunbin777@users.noreply.github.com>
2022-07-17 22:56:20 +0900

    cleanUp

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
 import { useState, useEffect } from "react";
 
+function Hello(){
+  useEffect(() => {
+    console.log("created :)");
+    return () => console.log("destroyed :(");
+  }, []);
+  return <h1>Hello</h1>;
+}
+
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  useEffect(() => {
-    console.log("I run only once.");
-  }, []); //두번째 argument가 react가 지켜봐야 할 것이기 때문에 처음 한번만 실행된다
-  useEffect(() => {
-    console.log("I run when 'keyword' changes.");
-  }, [keyword]); // keyword가 변화할 때만 실행된다
-  useEffect(() => {
-    console.log("I run when 'counter' changes.");
-  }, [counter]);
-  useEffect(() => {
-    console.log("I run when keyword or counter changes.");
-  }, [keyword, counter]);
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input value={keyword} onChange={onChange} type="text" placeholder="Searcg here" />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing? "Hide" : "Show"}</button>
     </div>
   );
 }
